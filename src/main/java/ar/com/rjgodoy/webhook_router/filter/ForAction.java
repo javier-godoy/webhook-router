89482443dfc664619f73ab364d7e476ba4c07e11
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2025-02-02 11:29:18 -0300

    chore: update license headers

--- src/main/java/ar/com/rjgodoy/webhook_router/filter/ForAction.java
+++ src/main/java/ar/com/rjgodoy/webhook_router/filter/ForAction.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2024 Roberto Javier Godoy
+ * Copyright (C) 2024-2025 Roberto Javier Godoy
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
