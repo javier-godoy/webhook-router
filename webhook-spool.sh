1622ca8a876397888d692230d6f2c857a41d1851
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2025-06-11 20:47:10 -0300

    chore: add webhook-spool script

---
+++ webhook-spool.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+java -jar /webhook-router.jar --config /webhook-router.cfg || sleep infinity
+echo -e "\nStart webhook monitor"
+
+SPOOL=/var/spool/webhook/
+java -jar /webhook-router.jar --config /webhook-router.cfg --hook $SPOOL
+
+while read FILE; do
+ java -jar /webhook-router.jar --config /webhook-router.cfg --hook $SPOOL$FILE
+done < <(inotifywait -qme MOVED_TO --format '%f' /var/spool/webhook/)
